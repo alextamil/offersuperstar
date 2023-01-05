93f093577b18b102e01c1cb042d62c623c1532e3
hochiminh <hochiminh.p@gmail.com>
2023-01-05 19:24:05 +0530

    chnages

---
+++ sripts/main.js
@@ -0,0 +1,41 @@
+function Productpage(){
+
+    var a = "product page.html";
+    window.location.href = a;
+}
+
+
+
+function offerstab(){
+
+    var a = "offerstab.html";
+    window.location.href = a;
+}
+
+
+function servicepage(){
+
+    var a = "service page.html";
+    window.location.href = a;
+}
+
+
+
+function offerspage(){
+
+    var a ="offer.html";
+    window.location.href = a;
+}
+
+
+function shopspage(){
+
+    var a ="shopspage.html";
+    window.location.href = a;
+}
+
+function services(){
+
+    var a ="services.html";
+    window.location.href = a;
+}
